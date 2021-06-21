b3afd32a5a310bcee50986c8d9035ccbb35db948
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2021-06-21 19:17:19 -0300

    Hello World

---
+++ HelloWorld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Brunno Lorenzoni
+      * Date: 2021-06-21 19:00
+      * Purpose: Hello World
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HelloWorld.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM HelloWorld.
